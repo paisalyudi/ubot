7f8456e95db9e05db894d501e67002dde49ed03e
paisalyudi <sultanadityaputra19@gmail.com>
2025-04-03 07:23:48 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,17 +5,17 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "99"))
 
-DEVS = list(map(int, os.getenv("DEVS", "7838767547").split()))
+DEVS = list(map(int, os.getenv("DEVS", "8074609099").split()))
 
 API_ID = int(os.getenv("API_ID", "23608953"))
 
 API_HASH = os.getenv("API_HASH", "253f5f31989038f81ebf234f008df79f")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7915132605:AAH3QmPkrY48-PkdD3POTUIbmqeoFgkZXIQ")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8143520741:AAF41uXaNdvSe8QZ_TD-ffDwn_yVAa7A5OU")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "7838767547"))
+OWNER_ID = int(os.getenv("OWNER_ID", "8074609099"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002399953106").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002458741546").split()))
 
 RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
